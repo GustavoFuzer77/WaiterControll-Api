2531f441f09743a3336a37ef8dc0908bc5cde664
Gustavo Fuzer <gustavofuzer256@gmail.com>
2023-11-06 17:30:39 -0300

    .

--- src/app/Repositories/ingredients/createIngredient.ts
+++ src/app/Repositories/ingredients/createIngredient.ts
@@ -4,17 +4,29 @@ import { Ingredients } from "../../models/Ingredients";
 
 export const createIngredient = async (req: Request, res: Response) => {
   try {
-    const icon = req.file?.filename
+    const icon = req.file?.filename;
     const { name } = req.body;
+
+    if (name && name.length <= 0) {
+      res.status(500).json({
+        error: true,
+        message: "O nome não pode ser vazio",
+      });
+    }
+
+    if (!icon) {
+      res.status(500).json({
+        error: true,
+        message: "O ícone não pode ser vazio",
+      });
+    }
 
     const ingredient = await Ingredients.create({ icon, name });
     res.status(201).json(ingredient);
   } catch (er) {
-    res
-      .status(500)
-      .json({
-        error: true,
-        message: "Ocorreu um erro ao tentar criar um ingrediente.",
-      });
+    res.status(500).json({
+      error: true,
+      message: "Ocorreu um erro ao tentar criar um ingrediente.",
+    });
   }
 };
